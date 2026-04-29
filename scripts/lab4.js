d0791ba230ee1e203bbb2b9882fd734dabeb3409
jhnkb <147684717+jhnkb@users.noreply.github.com>
2026-04-28 22:36:31 -0700

    Returning false in sumValues
    Checking data types in discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return add;
+        return false;
     }
 }
 
@@ -25,6 +25,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+
+    if (!Array.isArray(prices)) return false;
+    if (typeof discount !== 'number') return false;
 
     if (prices.length === 0) {
         return false;
